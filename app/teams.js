5f7162289f06cb9c2880118e19f3833370fbbc2b
shicks <shicks255@yahoo.com>
2018-10-05 23:54:13 -0400

    Initial commit

---
+++ app/teams.js
@@ -0,0 +1,126 @@
+
+const teams = [
+    {
+        "name": "Arizona Diamondbacks",
+        "team_id": "arizona-diamondbacks"
+    },
+    {
+        "name": "Atlanta Braves",
+        "team_id": "atlanta-braces"
+    },
+    {
+        "name": "Baltimore Orioles",
+        "team_id": "baltimore-orioles"
+    },
+    {
+        "name": "Boston Red Sox",
+        "team_id" : "boston-red-sox",
+    },
+    {
+        "name": "Chicago Cubs",
+        "team_id" : "chicago-cubs",
+    },
+    {
+
+        "name": "Chicago White Sox",
+        "team_id" : "chicago-white-sox",
+    },
+    {
+        "name": "Cincinnati Reds",
+        "team_id" : "cincinnati-reds",
+    },
+    {
+        "name": "Cleveland Indians",
+        "team_id" : "cleveland-indians",
+    },
+    {
+        "name": "Colorado Rockies",
+        "team_id" : "colorado-rockies",
+    },
+    {
+        "name": "Detroit tigers",
+        "team_id" : "detroit-tigers",
+    },
+    {
+        "name": "Houston Astros",
+        "team_id" : "houston-astros",
+    },
+    {
+        "name": "Kansas City Royals",
+        "team_id" : "kansas-city-royals",
+    },
+    {
+        "name": "Los Angeles Angels",
+        "team_id" : "los-angeles-angels",
+    },
+    {
+        "name": "Los Angeles Dodgers",
+        "team_id" : "los-angeles-dodgers",
+    },
+    {
+        "name": "Miami Marlins",
+        "team_id" : "miami-marlins",
+    },
+    {
+        "name": "Milwauke Brewers",
+        "team_id" : "milwaukee-brewers",
+    },
+    {
+        "name": "Minnesota Twins",
+        "team_id" : "minnesota-twins",
+    },
+    {
+        "name": "New York Mets",
+        "team_id" : "new-york-mets",
+    },
+    {
+        "name": "New York Yankees",
+        "team_id" : "new-york-yankees",
+    },
+    {
+        "name": "Oakland Athletics",
+        "team_id" : "oakland-athletics",
+    },
+    {
+        "name": "Philadelphia Phillies",
+        "team_id" : "philadelphia-phillies",
+    },
+    {
+        "name": "Pittsburgh Pirates",
+        "team_id" : "pittsburgh-pirates",
+    },
+    {
+        "name": "San Diego Padres",
+        "team_id" : "san-diego-padres",
+    },
+    {
+        "name": "San Francisco Giants",
+        "team_id" : "san-francisco-giants",
+    },
+    {
+        "name": "Seattle Mariners",
+        "team_id" : "seattle-mariners",
+    },
+    {
+        "name": "St. Louis Cardinals",
+        "team_id" : "st-louis-cardinals",
+    },
+    {
+        "name": "Tampa Bay Rays",
+        "team_id" : "tampa-bay-rays",
+    },
+    {
+        "name": "Texas Rangers",
+        "team_id" : "texas-rangers",
+    },
+    {
+        "name": "Toronto Blue Jays",
+        "team_id" : "toronto-blue-jays",
+    },
+    {
+        "name": "Washington Nationals",
+        "team_id" : "washington-nationals",
+    }
+]
+
+export {teams};
